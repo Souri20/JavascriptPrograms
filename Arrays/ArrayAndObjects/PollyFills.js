db29883a59d057c039bee7bd34e091835ee136e2
Souri20 <139069913+Souri20@users.noreply.github.com>
2025-02-22 07:13:01 +0530

    Update PollyFills.js

--- Arrays/ArrayAndObjects/PollyFills.js
+++ Arrays/ArrayAndObjects/PollyFills.js
@@ -10,4 +10,22 @@ Array.prototype.myMap = function(cb){
 
 let arr = [1,2,3,4,5];
 
-console.log(arr.myMap((item)=> item + 2))
+console.log(arr.myMap((item)=> item + 2));
+
+
+Array.prototype.myReduce = function(cb, initialValue){
+    
+    var acc = initialValue ;
+    for(let i=0; i<this.length ; i++){
+        
+        acc = acc ? cb(acc,this[i], i, this) : this[i]
+    }
+    
+    return acc;
+}
+
+let res = arr.myReduce((acc, cur)=>{
+    return acc+ cur.id
+}, 5)
+
+console.log(res)
